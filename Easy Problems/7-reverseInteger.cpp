fdfdfb93db00732433742a6fa85ea90e1bb78ad9
Utkarsh Sharma <skhiearth@gmail.com>
2020-02-07 23:31:21 +0530

    Create 7-reverseInteger.cpp

---
+++ Easy Problems/7-reverseInteger.cpp
@@ -0,0 +1,38 @@
+/*
+7. Reverse Integer
+Given a 32-bit signed integer, reverse digits of an integer.
+
+Example 1:
+    Input: 123
+    Output: 321
+
+Example 2:
+    Input: -123
+    Output: -321
+
+Example 3:
+    Input: 120
+    Output: 21
+
+Note:
+Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. 
+For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+*/
+
+#include <stdio.h>
+#include <limits.h>
+
+class Solution {
+public:
+    int reverse(int x) {
+        signed int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x = x / 10;
+            if (rev > INT_MAX/10 || (rev == INT_MAX / 10 && pop > 7)) return 0;
+            if (rev < INT_MIN/10 || (rev == INT_MIN / 10 && pop < -8)) return 0;
+            rev = pop + rev * 10;
+        }
+        return rev;
+    }
+};
